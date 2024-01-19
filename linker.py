b024ff6070d960a1b989531feb66e8cae2042359
Silvia Rus <47898287+Silvia-Rus@users.noreply.github.com>
2024-01-19 01:54:45 -0300

    is writing!

--- linker.py
+++ linker.py
@@ -1,7 +1,5 @@
 from pymarc import MARCReader
 from getters import getFieldDollarA
-from getters import getFieldDollarB
-from getters import getFieldDollarD
 from getters import getNRSubfield
 from getters import getHasUnlinkedAuth
 from getters import getFields
@@ -23,10 +21,11 @@ def link_auth(bibRecord, field, auth):
   global unlinkedAuth
   global matchingAuth
   global recordCounter
-  listFields = getFields(bibRecord, field) #toma TODOS los encabezamientos del 650
+  listFields = getFields(bibRecord, field) 
   i = 0   
-  for fieldInList in listFields:          
-    if getHasUnlinkedAuth(fieldInList):    #filtra los SIN $9
+  for fieldInList in listFields: 
+    #FILTERING         
+    if getHasUnlinkedAuth(fieldInList):  
       unlinkedAuth += 1
 
       #PREPARING
@@ -38,16 +37,20 @@ def link_auth(bibRecord, field, auth):
       results         = matchAuth[0]
       subfieldTwoText = matchAuth[1]
          
-      #WRITING
+      #PREPARING
       if len(results) > 0:
          result = results[0]
          print(logInfoDB(auth, result, subfieldTwo))
+         bibRecord.get_fields(field)[i].add_subfield('9', str(result[0]))
          matchingAuth += 1
       else:
          writeCSV(field.encode('utf-8'),biblionumber.encode('utf-8'), dollarA, subfieldTwoText)
          print("No matching authorities.")
       print("-----------")
     i += 1
+  #WRITTING
+  with open('prueba14.mrc', 'a') as out:
+    out.write(record.as_marc())
   recordCounter = recordCounter+1
 
 def subfieldTwoReturn(field):
@@ -81,8 +84,7 @@ def logInfoDB(auth, result, subfieldTwo):
   if str(result[3].encode('utf-8')) != '' : 
      text += "$"+subfieldTwo+": "+str(result[3].encode('utf-8'))
   return text
-
-     
+   
 def link_perso_100(record):
     link_auth(record, '100', '100')
 
@@ -104,11 +106,12 @@ with open(biblios, 'rb') as fh:
     reader = MARCReader(fh)
     for record in reader:
       # link_perso_100(record)
-      link_perso_700(record)
-      # link_topic_650(record)
+      # link_perso_700(record)
+      link_topic_650(record)
       # link_corpo_110(record)
       # link_corpo_710(record)
     print_resume()
+
   
     
   
